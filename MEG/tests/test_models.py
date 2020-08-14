555072da72af919b3585cf915cf20def6b9092c1
MatteoAnelli <matteo.anelli@alumnos.upm.es>
2020-08-14 18:45:33 +0300

    New Features: SCNN_swap architecture tested

---
+++ MEG/tests/test_models.py
@@ -0,0 +1,17 @@
+import sys
+import torch
+import MEG.dl.models as models
+
+def test_LeNet5_shape():
+
+    net = models.SCNN_swap()
+
+    x = torch.zeros([10, 1, 204, 20001])
+
+    with torch.no_grad():
+        print("Shape of the input tensor: {}".format(x.shape))
+
+        y = net(x)
+        assert y.shape == torch.Size([x.shape[0]]), "Bad shape of y: y.shape={}".format(y.shape)
+
+    print("Test LeNet5 output shape: Success.")
