a0ee12ede2c4d34386f1e8fc6a7b6eadbfa693d2
MatteoAnelli <matteo.anelli@alumnos.ump.es>
2020-11-06 16:46:04 +0200

    Train on Volta and Pascal GPU

--- example_batch_run_from_csv.sh
+++ example_batch_run_from_csv.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#SBATCH --time=03:30:00
+#SBATCH --time=01:10:00
 #SBATCH --mem-per-cpu=30000M
 #SBATCH --cpus-per-task=1
-#SBATCH --array=1-4
-#SBATCH --output=/scratch/work/anellim1/MEG-prediction/slurm/t1_out_%A_%a.log
+#SBATCH --array=6-11
+#SBATCH --output=/scratch/work/anellim1/MEG-prediction/slurm/ResNet_%A_%a.log
 #SBATCH --gres=gpu:1
+#SBATCH --constraint='pascal|volta'
 
 n=$(($SLURM_ARRAY_TASK_ID + 1))
 iteration=`sed -n "${n} p" parameters.csv`
@@ -42,3 +43,4 @@ mkdir -p tmp/$SLURM_ARRAY_TASK_ID
 
 
 srun python MEG/dl/DL_MEG.py --data_dir $data --figure_dir $figures --model_dir tmp/$SLURM_ARRAY_TASK_ID --sub $sub --hand $hand --batch_size $bs --batch_size_valid $bsv --batch_size_test $bst --epochs $epochs --learning_rate $lr --duration $duration --overlap $overlap --patience $patience --y_measure $y --experiment $exp --s_n_layer $snl --s_kernel_size $skern --t_n_layer $tnl --t_kernel_size $tkern --max_pooling $maxp --ff_n_layer $ffnl --ff_hidden_channels $ffhc --dropout $drop --activation $act
+
