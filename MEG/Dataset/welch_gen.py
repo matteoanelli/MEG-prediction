89b4897071fc41490a6a9f6713fafc757418a495
MatteoAnelli <matteanelli.96.11@gmail.com>
2021-04-17 12:34:54 +0300

    Implemented psd welch data generation.

---
+++ MEG/Dataset/welch_gen.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import os, sys
+import argparse
+import numpy as np
+from mne.time_frequency import psd_array_welch
+sys.path.insert(1, r"")
+
+from MEG.Utils.utils import *
+
+def generate_welch(data, fmin=1, fmax=70, fs=250, n_jobs=1):
+
+    psds, freqs = psd_array_welch(data, fs, fmin, fmax, n_per_seg=int(fs/2),
+                          n_overlap=int(fs/4), n_jobs=n_jobs)
+
+    return psds
+
+def main(args):
+
+    data_dir = args.data_dir
+
+    sub = args.sub
+    hand = args.hand
+
+
+    if hand == 0:
+        file_name = "sub_{}_left.npz".format(str(sub))
+        print("processing file :", file_name)
+        out_file = "sub_{}_left_welch.npz".format(str(sub))
+        print("output_file: ", out_file)
+    else:
+        file_name = "sub_{}_right.npz".format(str(sub))
+        print("processing file :", file_name)
+        out_file = "sub_{}_right_welch.npz".format(str(sub))
+        print("output_file: ", out_file)
+
+    dataset = np.load(os.path.join(data_dir, file_name))
+
+    X_train = dataset["X_train"]
+    X_train = np.swapaxes(X_train, 2, -1)
+    X_val = dataset["X_val"]
+    X_val = np.swapaxes(X_val, 2, -1)
+    X_test = dataset["X_test"]
+    X_test = np.swapaxes(X_test, 2, -1)
+
+
+
+    welch_train = generate_welch(X_train.squeeze(), fs=250, fmin=1,
+                                 fmax=70, n_jobs=1)
+    print("welch data shape: ", welch_train.shape)
+    print("train_done")
+
+    welch_val = generate_welch(X_val.squeeze(), fs=250, fmin=1,
+                                 fmax=70, n_jobs=1)
+    print("valid_done")
+
+    welch_test = generate_welch(X_test.squeeze(), fs=250, fmin=1,
+                                 fmax=70, n_jobs=1)
+    print("test_done")
+
+    np.savez(os.path.join(data_dir, out_file), welch_train=welch_train,
+             welch_val=welch_val, welch_test=welch_test)
+
+
+
+if __name__ == "__main__":
+    # main(sys.argv[1:])
+
+    parser = argparse.ArgumentParser()
+
+    # Directories
+    parser.add_argument('--data_dir', type=str, default='Z:\Desktop\\',
+                        help="Input data directory (default= Z:\Desktop\\)")
+    parser.add_argument('--sub', type=int, default='8',
+                        help="Input data directory (default= 8)")
+    parser.add_argument('--hand', type=int, default='0',
+                        help="hand (default= 0)")
+
+
+    args = parser.parse_args()
+
+    main(args)
--- example_batch_run.sh
+++ example_batch_run.sh
@@ -23,6 +23,7 @@ esac
 echo "sub is $sub"
 
 # srun python MEG/Dataset/create_preprocessed_dataset.py --data_dir /m/nbe/scratch/strokemotor/healthy_trans/ --out_dir /m/nbe/scratch/strokemotor/healthy_trans/preprocessed/ --sub $sub
-srun python MEG/bp_gen.py --data_dir /m/nbe/scratch/strokemotor/healthy_trans/preprocessed/ --sub $sub
+# srun python MEG/bp_gen.py --data_dir /m/nbe/scratch/strokemotor/healthy_trans/preprocessed/ --sub $sub
 #  srun python MEG/Dataset/plot_y_distr.py --data_dir /m/nbe/scratch/strokemotor/healthy_trans/preprocessed/ --figure_dir /scratch/work/anellim1/Figures --sub $sub --hand 1
 # srun python MEG/Dataset/PSD_plot.py --data_dir /m/nbe/scratch/strokemotor/healthy_trans/ --figure_dir /scratch/work/anellim1/Figures --sub $sub
+srun python MEG/Dataset/welch_gen.py --data_dir /m/nbe/scratch/strokemotor/healthy_trans/preprocessed/ --sub $sub
